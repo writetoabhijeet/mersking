6c9d0fc2e0e6cbc75e2fdf3cea6b3e173c446c44
Abhijeet Deshmukh <writedeshmukh@live.com>
2025-02-03 12:34:15 +0530

    Update MaskingStrategyFactory.java

--- src/main/java/com/example/mesker/MaskingStrategyFactory.java
+++ src/main/java/com/example/mesker/MaskingStrategyFactory.java
@@ -1,23 +1,24 @@
 package com.example.mesker;
 
+ import java.util.List;
+
 public class MaskingStrategyFactory {
 
-    public static MaskingStrategy getStrategy(MaskingStrategyType strategy, Object params) {
+    public static MaskingStrategy getStrategy(MaskingStrategyType strategy, Object params, char maskChar) {
         switch (strategy) {
             case REPLACE:
-                return new ReplaceMaskingStrategy();
+                return new ReplaceMaskingStrategy(maskChar);
             case HASH:
                 return new HashMaskingStrategy();
             case UNMASK_LAST_N_CHARS:
-                return new UnmaskLastNCharsStrategy((int) params);
+                return new UnmaskLastNCharsStrategy((int) params, maskChar);
             case UNMASK_FIRST_N_CHARS:
-                return new UnmaskFirstNCharsStrategy((int) params);
+                return new UnmaskFirstNCharsStrategy((int) params, maskChar);
             case UNMASK_FIRST_AND_LAST_N_CHARS:
                 List<Integer> paramList = (List<Integer>) params;
-                return new UnmaskFirstAndLastNCharsStrategy(paramList.get(0), paramList.get(1));
+                return new UnmaskFirstAndLastNCharsStrategy(paramList.get(0), paramList.get(1), maskChar);
             default:
                 throw new IllegalArgumentException("Unsupported masking strategy: " + strategy);
         }
     }
 }
-
