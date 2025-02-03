dc8044d841707ea0e261109108fd3f4c1299f6f7
Abhijeet Deshmukh <writedeshmukh@live.com>
2025-02-03 12:28:12 +0530

    Update SensitiveFieldConfig.java

--- src/main/java/com/example/mesker/SensitiveFieldConfig.java
+++ src/main/java/com/example/mesker/SensitiveFieldConfig.java
@@ -2,15 +2,21 @@ package com.example.mesker;
 
 import java.util.regex.Pattern;
 
+
+
+import java.util.regex.Pattern;
+
 public class SensitiveFieldConfig {
     private final Pattern pattern;
     private final MaskingStrategyType strategy;
     private final Object params;
+    private final char maskChar;
 
-    public SensitiveFieldConfig(Pattern pattern, MaskingStrategyType strategy, Object params) {
+    public SensitiveFieldConfig(Pattern pattern, MaskingStrategyType strategy, Object params, char maskChar) {
         this.pattern = pattern;
         this.strategy = strategy;
         this.params = params;
+        this.maskChar = maskChar;
     }
 
     public Pattern getPattern() {
@@ -24,5 +30,8 @@ public class SensitiveFieldConfig {
     public Object getParams() {
         return params;
     }
-}
 
+    public char getMaskChar() {
+        return maskChar;
+    }
+}
